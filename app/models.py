5a06be92bad11c781eeed81d243b8b609f90495e
Sadguru Chenu <sadgurustreak@gmail.com>
2026-03-15 16:52:31 +0530

    Add authentication models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+from app.database import Base
 
 class Thought(Base):
     __tablename__ = "thoughts"
@@ -7,3 +8,18 @@ class Thought(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True, nullable=False)
     content = Column(String, nullable=False)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+
+    hashed_password = Column(String, nullable=False)
+
+    is_active = Column(Boolean, default=True)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
